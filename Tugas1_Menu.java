c5f3383c6d29a34f643218cbdfa8c10cf65192a6
kaifaauladana23-pixel <kaifaauladana23@gmail.com>
2026-05-03 20:13:58 +0700

    Create Tugas1_Menu.java

---
+++ Tugas1_Menu.java
@@ -0,0 +1,11 @@
+class Menu {
+    String nama;
+    double harga;
+    String kategori;
+
+    Menu(String nama, double harga, String kategori) {
+        this.nama = nama;
+        this.harga = harga;
+        this.kategori = kategori;
+    }
+}
